257146bc8faa68c84a3a15310278ed3440baf291
Gabriel José <thirdendeal@protonmail.com>
2022-05-14 08:04:16 -0300

    Update algorithm test

--- test/algorithm.lua
+++ test/algorithm.lua
@@ -3,36 +3,17 @@
 --
 -- Usage: lua ./test/algorithm.lua <name> [scenario = worst] [size = 10]
 
-local package = require("src/algorithm/" .. arg[1])
-local mock = require("test/mock/" .. package.input)
+local report = require("test/suit/report")
 
+local name = arg[1]
 local scenario = arg[2] or "worst"
 local size = arg[3] or 10
 
+local package = require("src/algorithm/" .. name)
+local mock = require("test/suit/mock/" .. package.input)
+
 local input = mock[package[scenario]](size)
-local profile = package.algorithm(input)
 
 -- ---------------------------------------------------------------------
 
-local length = 0
-local sum = 0
-
-for key, value in pairs(profile) do
-  if #key > length then
-    length = #key
-  end
-
-  sum = sum + value
-end
-
-local format = "%-" .. length .. "s \t%i\n"
-
--- ---------------------------------------------------------------------
-
-for operation, count in pairs(profile) do
-  io.write(string.format(format, operation, count))
-end
-
-print(string.rep("-", length))
-
-io.write(string.format(format, "step", sum))
+report(package.algorithm(input))
---
+++ test/suit/report.lua
@@ -0,0 +1,44 @@
+-- Report
+-- ---------------------------------------------------------------------
+
+-- Private
+-- ---------------------------------------------------------------------
+
+local function printf(...)
+  io.write(string.format(...))
+end
+
+local function tally(profile)
+  local max_length = 0
+  local sum = 0
+
+  for key, value in pairs(profile) do
+    if #key > max_length then
+      max_length = #key
+    end
+
+    sum = sum + value
+  end
+
+  return max_length, sum
+end
+
+-- Public
+-- ---------------------------------------------------------------------
+
+local function report(profile)
+  local length, sum = tally(profile)
+
+  local format = "%-" .. length .. "s\t%i\n"
+
+  for operation, count in pairs(profile) do
+    printf(format, operation, count)
+  end
+
+  print()
+  printf(format, "step", sum)
+end
+
+-- ---------------------------------------------------------------------
+
+return report
