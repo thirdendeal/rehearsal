1976bfe9bb572f58be810456c3f763a0f5a93fef
Gabriel José <thirdendeal@protonmail.com>
2022-05-23 01:23:40 -0300

    Restructure the test suit

--- test/profile/sort.lua
+++ test/profile/sort.lua
@@ -3,7 +3,7 @@
 --
 -- Usage: lua ./test/algorithm.lua <name> [scenario = worst] [size = 10]
 
-local report = require("test/suit/report")
+local report = require("test/profile/suit/report")
 
 local name = arg[1]
 local scenario = arg[2] or "worst"
@@ -11,7 +11,7 @@ local size = tonumber(arg[3]) or 10
 local steps = tonumber(arg[4]) or 0
 
 local package = require("src/algorithm/" .. name)
-local mock = require("test/suit/mock/" .. package.input)
+local mock = require("test/profile/suit/mock/" .. package.input)
 
 local input = mock[package[scenario]](size)
 
