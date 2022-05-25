619e9a8d22a6b043eaaf04ebedcc32655a11693b
Gabriel José <thirdendeal@protonmail.com>
2022-05-25 10:35:36 -0300

    Implement a Doubly Linked List

---
+++ src/data-structure/doubly-linked-list.lua
@@ -0,0 +1,159 @@
+-- Doubly Linked List
+-- ---------------------------------------------------------------------
+
+local DoublyLinkedList = {}
+
+DoublyLinkedList.__index = DoublyLinkedList
+
+-- ---------------------------------------------------------------------
+
+function DoublyLinkedList:new()
+  local doubly_linked_list = {
+    head = nil,
+    tail = nil
+  }
+
+  return setmetatable(doubly_linked_list, self)
+end
+
+-- Node
+-- ---------------------------------------------------------------------
+
+local Node = {}
+
+Node.__index = Node
+
+-- ---------------------------------------------------------------------
+
+function Node:new(backward, data, forward, reverse)
+  if reverse then
+    backward, forward = forward, backward
+  end
+
+  local node = {
+    backward = backward,
+    data = data,
+    forward = forward
+  }
+
+  return setmetatable(node, self)
+end
+
+-- Private
+-- ---------------------------------------------------------------------
+
+local function directions(reverse)
+  local map = {
+    after = "forward",
+    before = "backward",
+    head = "head",
+    tail = "tail"
+  }
+
+  if reverse then
+    map.head, map.tail = map.tail, map.head
+
+    map.after, map.before = map.before, map.after
+  end
+
+  return map
+end
+
+-- Public
+-- ---------------------------------------------------------------------
+
+function DoublyLinkedList:delete_at(index, reverse)
+  local replace = self:read(index, reverse)
+
+  if replace then
+    local previous = replace.backward
+    local following = replace.forward
+
+    if previous then
+      previous.forward = following
+    end
+
+    if following then
+      following.backward = previous
+    end
+
+    if not previous and not following then
+      self.head = nil
+      self.tail = nil
+    end
+  end
+end
+
+function DoublyLinkedList:insert_at(index, value, reverse)
+  local map = directions(reverse)
+
+  if index == 1 and not self.head then
+    self.head = Node:new(nil, value, nil)
+
+    self.tail = self.head
+  elseif index == 1 then
+    local node = Node:new(nil, value, self[map.head], reverse)
+
+    self[map.head][map.before] = node
+    self[map.head] = node
+  else
+    local previous = self:read(index - 1, reverse)
+
+    if previous then
+      local node = Node:new(previous, value, previous[map.after], reverse)
+
+      if previous[map.after] then
+        previous[map.after][map.before] = node
+      else
+        self[map.tail] = node
+      end
+
+      previous[map.after] = node
+    end
+  end
+end
+
+function DoublyLinkedList:read(index, reverse)
+  local count = 1
+
+  for node in self:traverse(reverse) do
+    if index == count then
+      return node
+    end
+
+    count = count + 1
+  end
+end
+
+function DoublyLinkedList:search(value, reverse)
+  local index = 1
+
+  for node in self:traverse(reverse) do
+    if node.data == value then
+      return index
+    end
+
+    index = index + 1
+  end
+end
+
+function DoublyLinkedList:traverse(reverse)
+  local current = {
+    backward = self.tail,
+    forward = self.head
+  }
+
+  local step = reverse and "backward" or "forward"
+
+  return function()
+    current = current[step]
+
+    if current then
+      return current
+    end
+  end
+end
+
+-- ---------------------------------------------------------------------
+
+return DoublyLinkedList
---
+++ test/case/data-structure/doubly-linked-list.lua
@@ -0,0 +1,33 @@
+-- Doubly Linked List Test Case
+-- ---------------------------------------------------------------------
+
+local DoublyLinkedList = require("src/data-structure/doubly-linked-list")
+
+local doubly_linked_list = DoublyLinkedList:new()
+
+-- ---------------------------------------------------------------------
+
+-- Prepend
+
+doubly_linked_list:insert_at(1, 2)
+doubly_linked_list:insert_at(1, 3)
+doubly_linked_list:insert_at(1, 5)
+doubly_linked_list:insert_at(1, 7)
+
+-- Append - At the first empty index
+
+doubly_linked_list:insert_at(5, 11)
+doubly_linked_list:insert_at(6, 11, true)
+
+-- Remove
+
+doubly_linked_list:delete_at(2)
+
+-- ---------------------------------------------------------------------
+
+assert(doubly_linked_list:search(5) == 2)
+assert(doubly_linked_list:read(2).data == 5)
+
+for node in doubly_linked_list:traverse() do
+  print(node.data)
+end
