a092a34fb42fbc8fdfda9648bcc22dc8183fe874
Gabriel José <thirdendeal@protonmail.com>
2022-05-26 07:14:42 -0300

    Implement a Queue

---
+++ source/abstract-data-type/queue.lua
@@ -0,0 +1,38 @@
+-- Queue
+-- ---------------------------------------------------------------------
+
+local DoublyLinkedList = require("source/data-structure/doubly-linked-list")
+
+-- ---------------------------------------------------------------------
+
+local Queue = {}
+
+Queue.__index = Queue
+
+-- ---------------------------------------------------------------------
+
+function Queue:new()
+  local queue = {
+    dll = DoublyLinkedList:new()
+  }
+
+  return setmetatable(queue, self)
+end
+
+-- ---------------------------------------------------------------------
+
+function Queue:dequeue()
+  local node = self.dll:delete_at(1, true)
+
+  if node then
+    return node.data
+  end
+end
+
+function Queue:enqueue(value)
+  self.dll:insert_at(1, value)
+end
+
+-- ---------------------------------------------------------------------
+
+return Queue
---
+++ test/abstract-data-type/queue.lua
@@ -0,0 +1,29 @@
+-- Queue Test Case
+-- ---------------------------------------------------------------------
+
+local Queue = require("source/abstract-data-type/queue")
+
+local queue = Queue:new()
+
+-- State 1: Empty
+-- ---------------------------------------------------------------------
+
+assert(queue:dequeue() == nil) -- Queue: (empty)
+
+queue:enqueue(1) -- Queue: 1
+
+-- State 2: One element
+-- ---------------------------------------------------------------------
+
+assert(queue:dequeue() == 1) -- Queue: (empty)
+
+queue:enqueue(2) -- Queue: 2
+queue:enqueue(3) -- Queue: 3, 2
+
+-- State 3: More than one element
+-- ---------------------------------------------------------------------
+
+assert(queue:dequeue() == 2) -- Queue: 3
+
+queue:enqueue(4) -- Queue: 4, 3
+queue:enqueue(5) -- Queue: 5, 4, 3
