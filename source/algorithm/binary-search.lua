d2ffe7b61f60403b3afaf1371f6bbb1bf9e6bb6d
Gabriel José <thirdendeal@protonmail.com>
2022-05-27 15:02:20 -0300

    Implement Binary Search

---
+++ source/algorithm/binary-search.lua
@@ -0,0 +1,34 @@
+-- Binary Search
+-- ---------------------------------------------------------------------
+
+-- Private
+-- ---------------------------------------------------------------------
+
+local function partiton(array, value, a, b)
+  if a > b then
+    return
+  end
+
+  local index = math.floor((a + b) / 2)
+
+  if array[index] == value then
+    return index
+  elseif array[index] < value then
+    a, b = index + 1, b
+  else
+    a, b = a, index - 1
+  end
+
+  return partiton(array, value, a, b)
+end
+
+-- Public
+-- ---------------------------------------------------------------------
+
+local function binary_search(sorted_array, value)
+  return partiton(sorted_array, value, 1, #sorted_array)
+end
+
+-- ---------------------------------------------------------------------
+
+return binary_search
