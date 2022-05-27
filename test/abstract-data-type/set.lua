58bb8250c92f8c4d9ec652a13daed171e6c93be1
Gabriel José <thirdendeal@protonmail.com>
2022-05-27 13:41:37 -0300

    Add Set test case

---
+++ test/abstract-data-type/set.lua
@@ -0,0 +1,39 @@
+-- Set Test Case
+-- ---------------------------------------------------------------------
+
+local equal_arrays = require("share.equal-arrays")
+
+-- ---------------------------------------------------------------------
+
+local Set = require("source.abstract-data-type.set")
+
+local set_1 = Set:new({1, 2, 3})
+local set_2 = Set:new()
+
+-- ---------------------------------------------------------------------
+
+set_2:add(3) -- Set 2: {3}
+set_2:add(4) -- Set 2: {3, 4}
+
+set_2:add(4) -- Set 2: {3, 4}
+
+-- ---------------------------------------------------------------------
+
+local difference_1 = set_1 - set_2
+local insersection = set_1 ^ set_2
+
+assert(difference_1 + insersection == set_1)
+
+-- ---------------------------------------------------------------------
+
+assert(set_2:is_member(4))
+
+set_2:remove(4) -- Set 2: {3}
+set_2:remove(4) -- Set 2: {3}
+
+assert(set_1:is_superset(set_2))
+assert(set_2:is_subset(set_1))
+
+-- ---------------------------------------------------------------------
+
+assert(equal_arrays(set_2:enumerate(), {3}))
